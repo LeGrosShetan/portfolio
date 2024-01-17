324ed6326d8cffaea9c5fd07e298d4aa790d11e9
LeGrosShetan <maxagaz2002@hotmail.fr>
2024-01-17 15:10:44 -0500

    Routing opérationnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {AccueilComponent} from "./accueil/accueil.component";
 import {HeaderComponent} from "./header/header.component";
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AccueilComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, AccueilComponent, HeaderComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {AboutComponent} from "./about/about.component";
+import {AccueilComponent} from "./accueil/accueil.component";
+import {ContactComponent} from "./contact/contact.component";
+import {PortfolioComponent} from "./portfolio/portfolio.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AccueilComponent,
+    title: 'Accueil'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'A Propos'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Contact'
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+    title: 'Portfolio'
+  }
+];
