a8b36f76e66c93983e57ee4da7a84a3ed3cf7c79
LeGrosShetan <maxagaz2002@hotmail.fr>
2024-01-18 14:26:16 -0500

    Taille Header dynamique + fix pb lies au position:fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterModule} from "@angular/router";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    NgStyle
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  protected readonly headerHeight = headerHeight;
 }
+
+export const headerHeight = "50px";
