1edb89b5f5ccc3a4c12c70acccf9d6259b3ad435
LeGrosShetan <maxagaz2002@hotmail.fr>
2024-02-04 23:35:55 -0500

    Download CV opérationnel

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  getCVPDF(){
 
+    var fileURL = "../assets/CV_Maxime_Poiret.pdf";
+    window.open(fileURL);
+    var a         = document.createElement('a');
+    a.href        = fileURL;
+    a.target      = '_blank';
+    a.download    = 'CV_Maxime_Poiret.pdf';
+    document.body.appendChild(a);
+    a.click();
+  }
 }
